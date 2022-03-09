fc5334583efbf193a57c08f12ea17c9550fe2cec
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-10 02:36:03 +0530

    icons

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ app.get("/",function (req,res) {
     const jp_url = "https://api.openweathermap.org/data/2.5/weather?q=Jaipur&appid=7516370ff6bc0e6814edb8044d80bfea&units=metric";
 
     https.get(jp_url, function (response) {
-        console.log(response);
-    });
+        console.log(response.statusCode);
 
-    res.send("Is this working?");
-})
+        response.on("data",function (data) {
+            var weather_data = JSON.parse(data);
+            var temp = weather_data.main.temp;
+            var ic = weather_data.weather[0].icon;
+            var imgURL = "http://openweathermap.org/img/wn/"+ic+"@2x.png";
+            console.log(temp);
+            console.log(weather_data.weather[0].description);
+            console.log(weather_data);
+             res.write("<h1> The temp in Jaipur is " + weather_data.main.temp + "</h1>");
+             res.write("<p> The weather description is "+weather_data.weather[0].description+"</p>");
+             res.write("<img src="+ imgURL +">");
+            res.send();
+        });
+    });
+});
 
 
 app.listen(3000,function () {
