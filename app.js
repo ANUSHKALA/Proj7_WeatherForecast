ec8677ad247858770862f2d28ba646d26b5db5ce
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-10 15:07:29 +0530

    weather for diff cities from form

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const https = require("https");
-const app = express();
+const bodyParser = require("body-parser");
 
+const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/",function (req,res) {
 
-    const jp_url = "https://api.openweathermap.org/data/2.5/weather?q=Jaipur&appid=7516370ff6bc0e6814edb8044d80bfea&units=metric";
+      res.sendFile(__dirname+"/index.html");
 
-    https.get(jp_url, function (response) {
+});
+
+app.post("/",function (req,res) {
+    console.log(req.body.city);
+
+
+    const query = req.body.city;
+    const key = "7516370ff6bc0e6814edb8044d80bfea";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+key+"&units=metric";
+
+
+    https.get(url, function (response) {
         console.log(response.statusCode);
 
         response.on("data",function (data) {
@@ -18,13 +31,14 @@ app.get("/",function (req,res) {
             console.log(temp);
             console.log(weather_data.weather[0].description);
             console.log(weather_data);
-             res.write("<h1> The temp in Jaipur is " + weather_data.main.temp + "</h1>");
+             res.write("<h1> The temp in "+query+" is " + weather_data.main.temp + "</h1>");
              res.write("<p> The weather description is "+weather_data.weather[0].description+"</p>");
              res.write("<img src="+ imgURL +">");
-            res.send();
+             res.send();
         });
     });
-});
+
+})
 
 
 app.listen(3000,function () {
